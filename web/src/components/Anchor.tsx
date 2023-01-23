fd05917c9a4c3f3470de6b8366afce4ba1938c7a
Pedro Henrique <contact.pedrolima@gmail.com>
2023-01-23 17:55:05 -0300

    added google auth

--- web/src/components/Anchor.tsx
+++ web/src/components/Anchor.tsx
@@ -6,7 +6,7 @@ interface IAnchor {
 export function Anchor({ to, children }: IAnchor) {
   return (
     <a
-      className="flex items-center justify-center text-ctp-subtext1 font-medium text-lg hover:text-ctp-green transition-colors"
+      className="flex items-center justify-center text-ctp-subtext1 font-medium text-lg hover:text-ctp-flamingo transition-colors"
       target="_self"
       href={to}
     >
--- web/src/components/LoginForm.tsx
+++ web/src/components/LoginForm.tsx
@@ -1,10 +1,40 @@
+import { useEffect, useState } from "react";
 import { Anchor } from "./Anchor";
 
+import jwt_decode from "jwt-decode";
+
 export function LoginForm() {
+
+  const [user, setuser] = useState<any>({});
+
+  const handleCallbackResponse = (response: any) => {
+    console.log(response.credential);
+
+    var userObject = jwt_decode(response.credential);
+  };
+
+  useEffect(() => {
+    /* global google */
+    // @ts-ignore
+    google.accounts.id.initialize({
+      client_id:
+        "635718201896-1v05hgpg57pj74e9709e6qh92ram5284.apps.googleusercontent.com",
+      callback: handleCallbackResponse,
+    });
+
+    // @ts-ignore
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+    // @ts-ignore
+    google.accounts.id.prompt();
+  }, []);
+
   return (
     <div className="flex flex-col items-center p-3 min-w-[350px] gap-4 min-h-[400px] rounded-lg shadow-2xl drop-shadow-2xl bg-ctp-surface0">
-      <div> google auth </div>
-      <form className="flex items-center gap-4 justify-center rounded-lg flex-col w-[90%] bg-ctp-surface1 p-4">
+      <div id="signInDiv" />
+      <form className="flex items-center gap-4 justify-center rounded-lg flex-col w-full p-4">
         <div className="flex flex-col w-full gap-6">
           <input
             className="p-4 rounded-md bg-ctp-surface2 border border-ctp-overlay1 text-ctp-text text-md font-medium placeholder:font-medium placeholder:text-ctp-subtext0"
