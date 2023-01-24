22e39bd3d53978a92ee154eee3e7de1b31ccca7a
Pedro Henrique <contact.pedrolima@gmail.com>
2023-01-24 14:33:38 -0300

    created get users route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -10,15 +10,36 @@ export async function appRoutes(app: FastifyInstance) {
 
   // get users
   app.get("/users", async () => {
-    const users = await prisma.user.findMany();
+    const users = await prisma.user.findMany({
+      select: {
+        id: true,
+        username: true,
+        email: true,
+        password: true,
+        pictureUrl: true,
+        created_at: true,
+        tasklists: {
+          select: {
+            id: true,
+            name: true,
+            userId: true,
+            created_at: true,
+            tasks: {
+              select: {
+                id: true,
+                name: true,
+                description: true,
+                completed: true,
+                created_at: true,
+                tasklistId: true,
+              },
+            },
+          },
+        },
+      },
+    });
 
     return users;
   });
 
-  // get tasklists
-  app.get("/tasklists", async () => {
-    const tasklists = await prisma.tasklist.findMany();
-
-    return tasklists;
-  })
 }
