09e3dbfcaf81c2d405e7de9c38d911d0768b4e70
Pedro Henrique <contact.pedrolima@gmail.com>
2023-02-02 14:36:02 -0300

    updated primary button animation

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -10,7 +10,7 @@ export function Button({ children, title, primary, ...rest }: IButton) {
   if (primary) {
     return (
       <button
-        className="p-3 rounded-lg border-b-4 bg-ctp-green w-1/2 font-semibold text-ctp-surface1 border border-ctp-surface1 text-lg hover:bg-ctp-green/80 hover:border-b-2 hover:translate-y-1 focus:outline-none focus:ring ring-offset-1 focus:border-none ring-ctp-mauve ring-offset-ctp-crust transition-all ease-in-out"
+        className="p-3 rounded-lg border-b-[6px] bg-ctp-green w-1/2 font-semibold text-ctp-surface1 border border-ctp-surface1 text-lg hover:bg-ctp-green/80 hover:text-white hover:border-b-2 hover:translate-y-1 focus:outline-none focus:ring ring-offset-1 focus:border-none ring-ctp-mauve ring-offset-ctp-crust transition-all ease-in-out"
         title={title}
         {...rest}
       >
