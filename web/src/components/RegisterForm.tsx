7e7a6aa2d15664ae466f1d1bf7ed2a28503d90e0
Pedro Henrique <contact.pedrolima@gmail.com>
2023-02-05 20:05:31 -0300

    created first structure for register form

--- web/src/components/RegisterForm.tsx
+++ web/src/components/RegisterForm.tsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
+import { TextInput } from "./form/TextInput";
+import { EmailInput } from "./form/EmailInput";
+import { PasswordInput } from "./form/PasswordInput";
+
 export function RegisterForm() {
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-    const handleCreateNewUser = () => {
+  // TODO: fix X axis paddings for inputs
 
-    }
+  const handleCreateNewUser = () => {};
 
-    return (
-        <div className="flex fixed flex-col items-center p-6 w-[350px] h-[420px] md:w-[700px] md:max-h-[500px] gap-4 rounded-lg shadow-4xl drop-shadow-2xl bg-ctp-crust bg-opacity-90 backdrop-blur-md transition-all">
-            <form action="" onSubmit={handleCreateNewUser}>
-
-            </form>
-        </div>
-    )
+  return (
+    <div className="flex fixed flex-col items-center p-6 w-[350px] h-[420px] md:w-[700px] md:max-h-[500px] gap-4 rounded-lg shadow-4xl drop-shadow-2xl bg-ctp-crust bg-opacity-90 backdrop-blur-md transition-all">
+      <h1 className="text-ctp-text font-semibold text-lg">
+        Register a account{" "}
+      </h1>
+      <form action="" onSubmit={handleCreateNewUser}>
+        <div className="flex flex-col w-full mt-4 mb-4 gap-6">
+          <TextInput
+            action={setName}
+            value={name}
+            placeholder="🤔 Name"
+          />
+          <EmailInput action={setEmail} value={email} placeholder={"Email"}/>
+          <PasswordInput action={setPassword} value={password} placeholder={"Password"}/>
+        </div> 
+      </form>
+    </div>
+  );
 }
--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -2,12 +2,17 @@ import { Header } from "../components/Header";
 import { RegisterForm } from "../components/RegisterForm";
 
 export function Register() {
-    return (
-        <div className="flex w-screen h-screen">
-            <Header />
-            <main className="flex flex-1 mt-14 items-center justify-center bg-ctp-base text-white">
-                <RegisterForm />
-            </main>
-        </div>
-    )
+  return (
+    <div className="flex w-screen h-screen">
+      <Header />
+      <main
+        style={{
+          backgroundImage: "url(/bg-texture-2.svg)",
+        }}
+        className="flex flex-1 mt-14 items-center justify-center bg-ctp-base text-white"
+      >
+        <RegisterForm />
+      </main>
+    </div>
+  );
 }
